bdd1be3585df08e35b887f373e7392d95cbceef9
petechukV <petechukvlad@gmail.com>
2017-03-21 18:34:04 +0200

    Olimp sp

---
+++ SP/SP/Source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <time.h>
+using namespace std;
+void main() {
+	/*const int n = 10;
+	int a[n], i, s = 0;
+	srand(time(NULL));
+	for (i = 0; i < n; i++)s += (!((a[i] = rand() % 10) %2 ));
+	cout << s;*///suma rand nepar
+	/*union tcod { char sim;
+	struct { int x : 5;
+	int y : 3;
+	}hh;
+	};
+	/////////////////////////////////////////
+	tcod cod;
+	cod.hh.x = 4;
+	cod.hh.y = 2;
+	cout << "\n"<<' '<<sizeof(cod)<<' '<<cod.sim;*///4d
+	system("pause >>void");
+}
