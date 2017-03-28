6fd9b1834af269e4010819c1bcd4c67fa107c0dc
petechukV <petechukvlad@gmail.com>
2017-03-28 18:20:26 +0300

    2008 year

--- SP/SP/Source.cpp
+++ SP/SP/Source.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 #include <time.h>
+#include <stdio.h>
 using namespace std;
-void main() {
+class M {
+	int x;
+public: M(int X) :x(X) {}M func(M&m) {
+	m.x *= x;
+	return *this;
+}
+		void disp() { cout << x << " "; }
+};
+int inc5(int &x) { return x + 1; }
+
+void main(void) {
+	int x5 = 10; cout << (inc5(x5) + 10) << " "; cout << x5 << endl;
+	//M a(10), b(20);
+	//a.func(b).disp();
+//	b.disp();
 	/*const int n = 10;
 	int a[n], i, s = 0;
 	srand(time(NULL));
@@ -17,7 +32,17 @@ void main() {
 	cod.hh.x = 4;
 	cod.hh.y = 2;
 	cout << "\n"<<' '<<sizeof(cod)<<' '<<cod.sim;*///4d
-	cout << "zavdany' za 2007" << "\n"; 
-
+	cout << "zavdany' za 2008" << "\n"; 
+/*	const int N=500, M=10;
+	int a[N], st[M];
+	int p, j, i;
+	for (i = 0; M - i; st[i++] = 0);
+	for (i = 0; N - 1; st[a[i++]]++);
+	for (p=i=0;N-i;i++)
+		for(j=-1;st[i]-++j;a[p++]=i)*/
+	/*int x = 0, y = 0, z = 0;
+	++y+=y+++z + --x + ++++y + x+++z;
+	cout << x <<' '<< y <<' '<< z;*/
+	
 	system("pause >>void");
 }
